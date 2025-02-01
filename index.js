750549b0578fd7e4678935cc568cf18880cd9bb6
Nevin B <nevinb60@gmail.com>
2025-02-01 00:14:09 -0600

    added functionality to history display

--- index.js
+++ index.js
@@ -1,27 +1,81 @@
 const currentDisplay = document.querySelector('.current');
 const numberButtons = document.querySelectorAll('.number');
-const MAX = 10;
+const operatorButtons = document.querySelectorAll('.operator');
+const historyDisplay = document.querySelector('.history');
+const MAX_LENGTH = 10;
+
+let current_value = [];
+let history_value = [];
+let operator = '';
+
+function updateCurrentDisplay() {
+    numberButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const value = button.getAttribute("data-value");
+            if (current_value.length < MAX_LENGTH) {
+                current_value.push(value);
+                currentDisplay.textContent = current_value.join("");
+            }
 
 
-numberButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const value = button.getAttribute("data-value");
-        currentDisplay.textContent += value;
+
+        })
     })
-})
+
+}
+
+updateCurrentDisplay();
+
+function updateHistoryDisplay() {
+    operatorButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const operand = button.getAttribute("data-value");
+            operator = operand;
+            history_value = current_value.join("");
+            historyDisplay.textContent = history_value;
+            current_value = [];
+
+        })
+    })
+}
+
+updateHistoryDisplay();
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    }
+    else {
+        console.error("Div by zero");
+
+    }
+}
 
 
 function operate(a, b, operand) {
     switch (operand) {
         case '+':
-            return a + b;
+            return add(a, b);
         case '-':
-            return a - b;
+            return subtract(a, b);
         case '/':
-            if (b === 0) throw new Error("Division by zero")
-            return a / b;
+            return divide(a, b);
         case '*':
-            return a * b;
+            return multiply(a, b);
 
     }
 
