58f018097e5dec072e9618cb78fa15358979883d
Nevin B <nevinb60@gmail.com>
2025-02-06 21:36:22 -0600

    added color to the css

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const currentDisplay = document.querySelector('.current');
 const historyDisplay = document.querySelector('.history');
-const MAX_LENGTH = 10;
+const MAX_LENGTH = 9;
 
 
 
 function appendToDisplay(input) {
-    currentDisplay.value += input
+    if (currentDisplay.value.length <= MAX_LENGTH) {
+        currentDisplay.value += input
 
-
-}
+    }
+};
 
 function evaluateOperation() {
     historyDisplay.textContent = `${currentDisplay.value} =`
     currentDisplay.textContent = eval(currentDisplay.value)
 
-
-}
-
+};
 
 
 function clearDisplay() {
+    historyDisplay.textContent = `Ans = ${currentDisplay.value}`
     currentDisplay.value = "";
+
 };
 
 function remove() {
     currentDisplay.value = currentDisplay.value.slice(0, -1);
-}
+};
 
 
 
